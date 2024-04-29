3770bba3583238a7a36ceb52185d01853df32eaf
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-04-29 21:23:39 +0300

    Corrected DbContext namespace

--- WebDavServer/Program.cs
+++ WebDavServer/Program.cs
@@ -1,29 +1,33 @@
 using Microsoft.EntityFrameworkCore;
-using WebDavServer.Context;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<WebDavDbContext>(options => options.UseSqlServer("Server=ALIMCHEZ;Database=WebDavDb;Trusted_Connection=True;TrustServerCertificate=True;"));
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<WebDavDbContext>(options => options.UseSqlServer("Server=ALIMCHEZ;Database=WebDavDb;Trusted_Connection=True;TrustServerCertificate=True;"));
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
