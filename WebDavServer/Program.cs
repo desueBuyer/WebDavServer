1da4cdee9c53603b4bd6212206b0c23a938981a7
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-04-29 20:49:18 +0300

    Corrected namespace

--- WebDavServer/Program.cs
+++ WebDavServer/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebDavServer.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<WebDavDbContext>(options => options.UseSqlServer("Server=ALIMCHEZ;Database=WebDavDb;Trusted_Connection=True;TrustServerCertificate=True;"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,3 +26,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
