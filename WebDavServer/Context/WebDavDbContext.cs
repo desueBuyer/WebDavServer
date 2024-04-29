d8e98e923c67335e45cb0b0ff069f2ba624dd560
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-04-29 20:48:58 +0300

    Corrected namespace

---
+++ WebDavServer/Context/WebDavDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebDavServer.Models;
+
+namespace WebDavServer.Context;
+
+public partial class WebDavDbContext : DbContext
+{
+    public WebDavDbContext()
+    {
+    }
+
+    public WebDavDbContext(DbContextOptions<WebDavDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Folder> Folders { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=ALIMCHEZ;Database=WebDavDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Folder>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Folders__3214EC070E2861E7");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Folders)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_Folders_Users1");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC076C66C080");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
