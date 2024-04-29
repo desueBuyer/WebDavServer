334aad83b40dc0eebc6be93c09e4c78343399d3b
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-04-29 20:41:51 +0300

    Addded Db models

---
+++ WebDavServer/Models/Folder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebDavServer.Models;
+
+public partial class Folder
+{
+    public int Id { get; set; }
+
+    public int? ParentFolderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? FolderName { get; set; }
+
+    public virtual User? User { get; set; }
+}
---
+++ WebDavServer/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebDavServer.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserPassword { get; set; }
+
+    public virtual ICollection<Folder> Folders { get; set; } = new List<Folder>();
+}
