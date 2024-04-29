78416af579d7e7577e3ab2dca6434f8afddd3b68
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-04-29 20:38:34 +0300

    Added FolderController

---
+++ WebDavServer/Controllers/FolderController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebDavServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FolderController : ControllerBase
+    {
+        WebDavDbContext db;
+
+
+        private readonly ILogger<FolderController> _logger;
+
+        public FolderController(ILogger<FolderController> logger, WebDavDbContext context)
+        {
+            _logger = logger;
+            db = context;
+            Console.WriteLine();
+        }
+
+        [HttpGet("user/{id}/folders")]
+        public async Task<ActionResult<IEnumerable<Folder>>> Get(int id)
+        {
+            var folders = await db.Folders.Where(x => x.UserId == id).ToListAsync();
+
+            if (folders.Count == 0)
+            {
+                return NotFound();
+            }
+            return folders;
+        }
+    }
+}
