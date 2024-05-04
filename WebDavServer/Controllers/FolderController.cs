4ee1a4e52b5d9fd597baf77f366f78899ee0d285
Alim-Lyukmanov <78745247+Alim-Lyukmanov@users.noreply.github.com>
2024-05-04 21:59:54 +0300

    Added method to search single user folder

--- WebDavServer/Controllers/FolderController.cs
+++ WebDavServer/Controllers/FolderController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using WebDavServer.Models;
+using WebDavServer.Context;
 
 namespace WebDavServer.Controllers
 {
@@ -12,8 +13,6 @@ namespace WebDavServer.Controllers
     public class FolderController : ControllerBase
     {
         WebDavDbContext db;
-
-
         private readonly ILogger<FolderController> _logger;
 
         public FolderController(ILogger<FolderController> logger, WebDavDbContext context)
@@ -28,11 +27,25 @@ namespace WebDavServer.Controllers
         {
             var folders = await db.Folders.Where(x => x.UserId == id).ToListAsync();
 
-            if (folders.Count == 0)
+            if (folders.Count() == 0)
             {
                 return NotFound();
             }
             return folders;
+        }
+
+        [HttpGet("user/{userId}/folder/{folderId}")]
+        public async Task<ActionResult<Folder>> Get(int userId, int folderId)
+        {    
+            try
+            {
+                Folder folder = await db.Folders.Where(x => x.UserId == userId && x.Id == folderId).FirstAsync();
+                return folder;
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
         }
     }
 }
